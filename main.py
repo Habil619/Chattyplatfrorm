ed2bfc975ab2722847eec247916eea8eb4f3432a
Habil619 <90960320+Habil619@users.noreply.github.com>
2023-09-15 20:54:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ from typing import List
 
 app = FastAPI()
 
-# Store active WebSocket connections
 active_connections: List[WebSocket] = []
 
-# WebSocket route for connecting to the chat
 @app.websocket("/ws/{user_id}")
 async def connect_websocket(websocket: WebSocket, user_id: int):
     await websocket.accept()
@@ -14,7 +12,6 @@ async def connect_websocket(websocket: WebSocket, user_id: int):
     try:
         while True:
             message = await websocket.receive_text()
-            # Broadcast the received message to all connected clients
             for connection in active_connections:
                 await connection.send_text(f"User {user_id}: {message}")
     except WebSocketDisconnect:
